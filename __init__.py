c26a7ed0e9f17295beb010a0bdc713f9053cf642
marianaimbper <marianaimbper@gmail.com>
2025-08-24 10:53:15 -0300

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,1 +1,14 @@
-from .label_map import sem2onehot
+# Copyright (c) Facebook, Inc. and its affiliates.
+from fvcore.transforms.transform import Transform, TransformList  # order them first
+from fvcore.transforms.transform import *
+from .transform import *
+from .augmentation import *
+from .augmentation_impl import *
+
+__all__ = [k for k in globals().keys() if not k.startswith("_")]
+
+
+from detectron2.utils.env import fixup_module_metadata
+
+fixup_module_metadata(__name__, globals(), __all__)
+del fixup_module_metadata
